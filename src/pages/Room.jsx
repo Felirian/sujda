6674290143accdd6dc081722816e23e5b02f91ad
shonenenenene <danya.sytaev@gmail.com>
2024-11-07 12:56:22 +0300

    edit: (data.js) -add mock data

--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
+import { styled } from 'styled-components';
 
-const Room = ({data}) => {
-  return (
-    <div>
-
-    </div>
-  );
+const Room = ({ data }) => {
+  return <RoomWr></RoomWr>;
 };
+
+const RoomWr = styled.div``;
 
 export default Room;
