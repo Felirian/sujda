a7afce9b666eb8b4b25158090d90a90d5417444e
Andrey Kristev <hhghv49@gmail.com>
2024-11-08 15:49:15 +0300

    fix: return (app.js) - map return

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import {Route, Routes} from 'react-router-dom';
 import Quiz from './pages/Quiz';
 import Room from './pages/Room';
 import Map from './pages/Map';
-import { ROOMS_DATA } from './features/data';
+import {ROOMS_DATA} from './features/data';
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<>gfdg</>} />
-      <Route path='map' element={<Map />} />
+      <Route path='/' element={<>gfdg</>}/>
+      <Route path='map' element={<Map/>}/>
       <Route path='room'>
-        {ROOMS_DATA.map((roomData, index) => {
-          return (
-            <Route key={index} path={roomData.route}>
-              <Route index element={<Room data={roomData} />} />
-              <Route path='quiz' element={<Quiz data={roomData.quiz} />} />
-            </Route>
-          );
-        })}
+        {ROOMS_DATA.map((roomData, index) => (
+          <Route key={index} path={roomData.route}>
+            <Route index element={<Room data={roomData}/>}/>
+            <Route path='quiz' element={<Quiz data={roomData.quiz}/>}/>
+          </Route>
+        ))}
       </Route>
     </Routes>
   );
