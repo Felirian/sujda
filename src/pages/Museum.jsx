c951f85af9ac23847e9f8e996d78121447efb512
petersizovdev <pitersizov@gmail.com>
2024-11-11 19:26:05 +0300

    upd (Museum)

--- src/pages/Museum.jsx
+++ src/pages/Museum.jsx
@@ -1,19 +1,56 @@
 import React from 'react';
-import Controls from "../components/Room/Controls";
-import styled from "styled-components";
-import Img from '../assets/rooms/secret/person3.png'
+import Controls from '../components/Room/Controls';
+import styled from 'styled-components';
+import Img from '../assets/rooms/secret/person3.png';
+import SvgSelector from '../components/Shared/SvgSelector';
+import ArrowButton from '../components/Shared/ArrowButton';
+import { Link } from 'react-router-dom';
 
 const Museum = () => {
   return (
-    <>
-      <Ganibal src={Img} alt={'fgdfd'}/>
-      <Controls />
-    </>
+    <MuseumWr>
+      <TopSection>
+        <SvgSelector svg={'elephantLogo'} />
+        <Link to={'/'}>
+          <ArrowButton>на главную</ArrowButton>
+        </Link>
+      </TopSection>
+      <ControlsWr>
+        <Ganibal src={Img} alt={'fgdfd'} />
+        <Controls />
+      </ControlsWr>
+    </MuseumWr>
   );
 };
 
+const MuseumWr = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  padding: 8.2vw 0vw;
+  height: 100vh;
+  width: 82vw;
+  margin: 0 auto;
+`;
+
+const TopSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 77vw;
+  gap: 4vw;
+`;
+
 const Ganibal = styled.img`
   width: 100%;
-`
+  display: block;
+`;
+
+const ControlsWr = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
 
 export default Museum;
