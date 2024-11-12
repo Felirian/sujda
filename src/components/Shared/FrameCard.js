cc97f5150ea41822189edcc9a5c3294b839f5100
pitersizovdev <pitersizov@gmail.com>
2024-11-12 15:34:13 +0300

    upd (shared) - add FrameCard

---
+++ src/components/Shared/FrameCard.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import styled from 'styled-components';
+import SvgSelector from './SvgSelector';
+import { COLORS } from '../../styles/variables';
+
+const FrameCard = ({ children }) => {
+  return (
+    <FrameCardWr>
+      <SvgSelector svg={'topframe'} />
+      <FrameCardContainer>
+        <FrameCardBox>{children}</FrameCardBox>
+      </FrameCardContainer>
+      <SvgSelector svg={'bottomframe'} />
+    </FrameCardWr>
+  );
+};
+
+export default FrameCard;
+
+const FrameCardWr = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 92vw;
+
+  & > svg {
+    width: 100%;
+    height: auto;
+  }
+`;
+
+const FrameCardContainer = styled.div`
+  background-color: ${COLORS.green};
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0vw 1.795vw;
+`;
+
+const FrameCardBox = styled.div`
+  background-color: ${COLORS.green};
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-right: 0.3vw solid ${COLORS.sand};
+  border-left: 0.3vw solid ${COLORS.sand};
+`;
