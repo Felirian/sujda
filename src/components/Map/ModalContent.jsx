36bd88d3d6be5b9584c19e7b6aad01363368e39d
shonenenenene <danya.sytaev@gmail.com>
2024-11-21 15:39:00 +0300

    edit: (Map) -add popups

---
+++ src/components/Map/ModalContent.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import LongFrameCard from '../Shared/LongFrameCard';
+import { styled } from 'styled-components';
+import { H1, P1 } from '../../styles/textTags';
+
+const ModalContent = ({ selectedPoint }) => {
+  return (
+    <LongFrameCard>
+      {selectedPoint && (
+        <ModalContentWr>
+          <Title>{selectedPoint.name}</Title>
+          <Info>{selectedPoint.info}</Info>
+          <ModalImg src={selectedPoint.img} alt={selectedPoint.name} />
+        </ModalContentWr>
+      )}
+    </LongFrameCard>
+  );
+};
+
+const ModalContentWr = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-top: 10vw;
+  padding-bottom: 10vw;
+  gap: 6.15vw;
+`;
+
+const Title = styled(H1)``;
+
+const Info = styled(P1)`
+  text-align: start;
+`;
+
+const ModalImg = styled.img``;
+
+export default ModalContent;
