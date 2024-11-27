99ad94166aa79def610e6efaa03ee44ea3895b42
shonenenenene <danya.sytaev@gmail.com>
2024-11-27 12:52:04 +0300

    edit: (data.js) -update exhibition data and room data

--- src/components/Map/MapSection/index.jsx
+++ src/components/Map/MapSection/index.jsx
@@ -5,13 +5,7 @@ import ZoomableMap from './ZoomableMap';
 
 const MapSection = ({ handleDotClick }) => {
   return (
-    <TransformWrapper
-      centerOnInit
-      initialScale={1}
-      doubleClick={{ disabled: true }}
-      maxScale={9}
-      minScale={1}
-    >
+    <TransformWrapper initialScale={1} doubleClick={{ disabled: true }} maxScale={9} minScale={1}>
       {({ zoomToElement }) => (
         <TransformComponent wrapperClass='map_wrapper' contentClass='map_wrapper_content'>
           <ZoomableMap handleDotClick={handleDotClick} zoomToElement={zoomToElement} />
