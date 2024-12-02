8e00bd90e05279a1552b977fca9cf774794a0c73
Daniil Sytaev <danya.sytaev@gmail.com>
2024-12-02 14:22:35 +0300

    edit: Fonts, Filter -change font -styling filter

--- src/components/Map/Filter/index.jsx
+++ src/components/Map/Filter/index.jsx
@@ -3,12 +3,10 @@ import styled from 'styled-components';
 import { COLORS } from '../../../styles/variables';
 import { FILTERS } from '../../../features/map';
 import SvgSelector from '../../Shared/SvgSelector';
-import { ButtonTextStyle } from '../../../styles/textTags';
+import { ButtonTextStyle, P2Style } from '../../../styles/textTags';
 
 const Filter = ({ selected, setSelected }) => {
   const [isOpen, setIsOpen] = useState(false);
-
-  console.log('rerererre');
 
   const toggleDropdown = () => setIsOpen((prev) => !prev);
 
@@ -35,7 +33,9 @@ const Filter = ({ selected, setSelected }) => {
               checked={selected.includes(index)}
               onChange={() => handleCheckboxChange(index)}
             />
-            <CustomCheckbox $checked={selected.includes(index)} />
+            <CustomCheckbox $checked={selected.includes(index)}>
+              {<SvgSelector svg='filterCheckbox' />}
+            </CustomCheckbox>
             {option}
           </ListItem>
         ))}
@@ -50,6 +50,7 @@ const DropdownWrapper = styled.div`
 `;
 
 const DropdownButton = styled.button`
+  position: relative;
   height: 11.28vw;
   width: 100%;
   padding: 2.05vw 4.1vw;
@@ -60,6 +61,7 @@ const DropdownButton = styled.button`
   justify-content: space-between;
   align-items: center;
   border-radius: 3.08vw;
+  z-index: 5;
 `;
 
 const DropdownTitle = styled.div`
@@ -69,32 +71,35 @@ const DropdownTitle = styled.div`
   left: 4.1vw;
   top: 50%;
   transform: translateY(-50%);
+  z-index: 10;
 `;
 
 const DropdownList = styled.div`
   position: absolute;
-  top: 100%;
+  padding-top: 5.13vw;
+  padding-bottom: 1.54vw;
+  top: 70%;
   left: 0;
   width: 100%;
-  border: 1px solid #ccc;
-  background-color: #fff;
+  background-color: ${COLORS.white};
+  opacity: 0.85;
   border-radius: 5px;
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-  max-height: 200px;
   overflow-y: auto;
   transform: ${({ $isopen }) => ($isopen ? 'scaleY(1)' : 'scaleY(0)')};
   transform-origin: top;
   transition: transform 0.2s ease-in-out;
-  z-index: 10;
+  z-index: 1;
 `;
 
 const ListItem = styled.label`
+  ${P2Style}
   display: flex;
   align-items: center;
-  padding: 8px 10px;
+  padding: 1.28vw 3.59vw;
   cursor: pointer;
 
-  color: ${COLORS.darkOrange};
+  color: ${COLORS.grey};
 
   &:hover {
     background-color: #f0f0f0;
@@ -102,24 +107,33 @@ const ListItem = styled.label`
 `;
 
 const CustomCheckbox = styled.div`
-  width: 16px;
-  height: 16px;
-  border: 2px solid ${({ $checked }) => ($checked ? '#007bff' : '#ccc')};
-  background-color: ${({ $checked }) => ($checked ? '#007bff' : '#fff')};
-  border-radius: 3px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin-right: 8px;
+  position: relative;
+  width: 4.1vw;
+  height: 4.1vw;
+  border: 1px solid ${COLORS.green};
+  border-radius: 0.77vw;
+  margin-right: 3.08vw;
 
-  &:after {
+  svg {
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%) scale(${({ $checked }) => ($checked ? '1' : '0')});
+    position: absolute;
+    width: 110%;
+    height: 110%;
+    transition: 0.1s;
+    opacity: ${({ $checked }) => ($checked ? 1 : 0)};
+    border-radius: 0.77vw;
+  }
+
+  /* &:after {
     content: '';
     width: 8px;
     height: 8px;
     background-color: #fff;
     display: ${({ $checked }) => ($checked ? 'block' : 'none')};
     border-radius: 2px;
-  }
+  } */
 `;
 
 const HiddenCheckbox = styled.input`
