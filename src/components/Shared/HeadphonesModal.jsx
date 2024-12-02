f38b68550215d0f3d2dc8bd200d58c7d5607bede
Daniil Sytaev <danya.sytaev@gmail.com>
2024-12-02 19:36:49 +0300

    edit: (HeadphonesModal) -change styles

--- src/components/Shared/HeadphonesModal.jsx
+++ src/components/Shared/HeadphonesModal.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { ButtonTextStyle, H1 } from '../../styles/textTags';
+import { ButtonTextStyle, H1, H3Style } from '../../styles/textTags';
 import SvgSelector from './SvgSelector';
 import { styled } from 'styled-components';
 import { COLORS } from '../../styles/variables';
 import FrameCard from './FrameCard';
+import CustomButton from './CustomButton';
 
 const HeadphonesModal = ({ modalIsOpen, handleModalChoice }) => {
   return (
@@ -14,12 +15,8 @@ const HeadphonesModal = ({ modalIsOpen, handleModalChoice }) => {
           <RoomModalContent>
             <H1>Включите, пожалуйста, наушники!</H1>
             <SvgSelector svg='headphones' />
-            <ModalBtn onClick={() => handleModalChoice('yes')}>
-              <ModalBtnContent>хорошо</ModalBtnContent>
-            </ModalBtn>
-            <ModalBtn onClick={() => handleModalChoice('no')}>
-              <ModalBtnContent>нет, спасибо</ModalBtnContent>
-            </ModalBtn>
+            <CustomButton size='medium' onClick={() => handleModalChoice('yes')} text='хорошо' />
+            <ModalBtn onClick={() => handleModalChoice('no')}>нет, спасибо</ModalBtn>
           </RoomModalContent>
         </FrameCard>
       </RoomModal>
@@ -33,7 +30,7 @@ const RoomBackdrop = styled.div`
   background-color: ${COLORS.black};
   opacity: 0.9;
   position: absolute;
-  z-index: 98;
+  z-index: 1000;
 `;
 
 const RoomModal = styled.div`
@@ -42,7 +39,7 @@ const RoomModal = styled.div`
   left: 50%;
   transform: ${({ $modalisopen }) => `translate(-50%, -50%) scale(${$modalisopen ? '1' : '0'})`};
   transition: transform 0.2s;
-  z-index: 99;
+  z-index: 1001;
 `;
 
 const RoomModalContent = styled.div`
@@ -51,23 +48,19 @@ const RoomModalContent = styled.div`
   justify-content: center;
   align-items: center;
   gap: 5.9vw;
+  h1 {
+    color: ${COLORS.yellow};
+  }
+  svg {
+    path {
+      fill: ${COLORS.white};
+    }
+  }
 `;
 
 const ModalBtn = styled.button`
-  padding: 0.77vw;
-  background-color: ${COLORS.sand};
-  margin-bottom: 0.77vw;
-`;
-
-const ModalBtnContent = styled.div`
-  ${ButtonTextStyle};
-  padding: 1.92vw 5.9vw;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border: 0.3vw solid ${COLORS.grey};
-  background-color: ${COLORS.sand};
-  height: 10.26vw;
+  ${H3Style};
+  color: ${COLORS.sand};
 `;
 
 export default HeadphonesModal;
