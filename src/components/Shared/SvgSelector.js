53e79763ed64507b55d6c094e69c2319c70fe896
Daniil Sytaev <danya.sytaev@gmail.com>
2024-12-02 13:27:01 +0300

    edit: Fonts -change font loading method to DATA URI

--- src/components/Shared/SvgSelector.js
+++ src/components/Shared/SvgSelector.js
@@ -554,6 +554,27 @@ const SvgSelector = ({ svg }) => {
         </svg>
       );
 
+    case 'filterArrow':
+      return (
+        <svg
+          width='20'
+          height='12'
+          viewBox='0 0 20 12'
+          fill='none'
+          xmlns='http://www.w3.org/2000/svg'
+        >
+          <g id='mark'>
+            <path
+              id='Vector 92'
+              d='M1 1.5L10 10.5L19 1.5'
+              stroke='#333E2C'
+              stroke-width='2'
+              stroke-linecap='round'
+            />
+          </g>
+        </svg>
+      );
+
     default:
       return <></>;
   }
